3495bbe0781f85e5aa595510a135cfb32c235014
v <vangvangvangvang1@gmail.com>
2023-11-06 12:00:49 +0100

    felt class

--- 22_CDIO_3/src/DiceSelecter.java
+++
@@ -1,7 +0,0 @@
-class DiceSelecter{
-    //Dont look ;(
-    public static void selectDice() {
-        System.out.println(System.lineSeparator() + "Du har valgt at bruge to terninger med 6 sider hver." + System.lineSeparator());
-    }
-}
-//Vi kunne ikke rigtigt finde ud af hvad der skulle blive gjort med dice selecting saa...
