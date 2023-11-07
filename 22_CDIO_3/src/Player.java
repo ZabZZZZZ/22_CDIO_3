d26fa47d13ce7b26fe880b43d0c010e38e1cc008
Zia Beg <ziabeg@hotmail.com>
2023-11-07 15:27:17 +0100

    New fields to Player

--- 22_CDIO_3/src/Player.java
+++ 22_CDIO_3/src/Player.java
@@ -1,23 +1,42 @@
 class Player {
 
-    private String name;
+    private String figure;
+    private int age;
+    private int currentField;
     private Account account;
 
-    public Player (String name) {
-        this.name = name;
-        this.account = new Account(1000);
+    //Constructor for figure and balance
+    public Player (String figure, int balance) {
+        this.figure = figure;
+        this.account = new Account(balance);
+        this.currentField = 0;
     }
 
-    public String getName() {
-        return this.name;
+    //Constructor for age
+    public Player(int age) {
+        this.age = age;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    //Getters
+    public String getFigure() {
+        return this.figure;
+    }
+
+    public int getAge() {
+        return this.age;
     }
 
     public int getPoints() {
         return account.GetBalance();
+    }
+
+    public int getCurrentField() {
+        return this.currentField;
+    }
+
+    //Setter
+    public void setFigure(String figure) {
+        this.figure = figure;
     }
 
     public void withdraw(int amount) {
