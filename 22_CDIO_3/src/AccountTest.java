d3f128435dc6fe64d6380017462f396df12d3cbb
Zia Beg <ziabeg@hotmail.com>
2023-11-24 12:00:13 +0100

    Added tests

--- 22_CDIO_3/src/AccountTest.java
+++ 22_CDIO_3/src/AccountTest.java
@@ -3,18 +3,24 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class AccountTest {
-  @Test
-  public void testWithdraw () {
-    Account account = new Account(1000);
+    @Test
+    public void testWithdraw () {
+        Account account = new Account(1000);
         account.withdraw(200);
         assertEquals(800, account.GetBalance());
     }
     
     @Test
-
     public void testDeposit() {
         Account account = new Account(1000);
         account.deposit(300);
         assertEquals(1300, account.GetBalance());
+    }
+
+    @Test
+    public void testForNoNegativeBalanceInAccount() {
+        Account account = new Account(1000);
+        account.withdraw(2000);
+        assertEquals(0, account.GetBalance());
     }
 }
